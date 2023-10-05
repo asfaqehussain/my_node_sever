2e5001f45c459907bfc1982707eccce66586a4d8
Asfaqe hussain <51645676+asfaqehussain@users.noreply.github.com>
2023-10-05 13:41:04 +0530

    remove the twilio

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 require('dotenv').config();
 
 const express = require('express');
-const AccessToken = require('twilio').jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
+// const AccessToken = require('twilio').jwt.AccessToken;
+// const ChatGrant = AccessToken.ChatGrant;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hey, this is my API running 🥳');
+  res.send(`Hey, this is my API running 🥳 ${process.env}`);
 });
 
-app.get('/token/:identity', (req, res) => {
-  const identity = req.params.identity;
-  const token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY,
-    process.env.TWILIO_API_SECRET
-  );
+// app.get('/token/:identity', (req, res) => {
+//   const identity = req.params.identity;
+//   const token = new AccessToken(
+//     process.env.TWILIO_ACCOUNT_SID,
+//     process.env.TWILIO_API_KEY,
+//     process.env.TWILIO_API_SECRET
+//   );
 
-  token.identity = identity;
-  token.addGrant(
-    new ChatGrant({
-      serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
-    })
-  );
+//   token.identity = identity;
+//   token.addGrant(
+//     new ChatGrant({
+//       serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
+//     })
+//   );
 
-  res.send({
-    identity: token.identity,
-    jwt: token.toJwt(),
-  });
-});
+//   res.send({
+//     identity: token.identity,
+//     jwt: token.toJwt(),
+//   });
+// });
 
-app.listen(3001, () => {
-  console.log('Programmable Chat server running on port 3001!');
-});
+// app.listen(3001, () => {
+//   console.log('Programmable Chat server running on port 3001!');
+// });
 
 
 
