8f0f928f0f3ffb8b44437adfe7c4442ccdbebba0
asfaqehussain <asfaqeh@gmail.com>
2023-10-05 12:11:21 +0530

    main file Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+// const Twilio = require('twilio');
+const express = require('express');
+// console.log('process.env', process.env);
+
+const app = express();
+const AccessToken = require('twilio').jwt.AccessToken;
+// const AccessToken = Twilio.jwt.AccessToken;
+const ChatGrant = AccessToken.ChatGrant;
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/token/:identity', (req, res) => {
+  const identity = req.params.identity;
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+  );
+  console.log('process.env', process.env);
+
+  token.identity = identity;
+  token.addGrant(
+    new ChatGrant({
+      serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
+    }),
+  );
+
+  res.send({
+    identity: token.identity,
+    jwt: token.toJwt(),
+  });
+});
+
+app.listen(3001, function () {
+  console.log('Programmable Chat server running on port 3001!');
+});
