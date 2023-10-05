d56d1252a8bc6b711eb4eac0e9a43567563157f7
Asfaqe hussain <51645676+asfaqehussain@users.noreply.github.com>
2023-10-05 13:01:46 +0530

    resolve the error

--- index.js
+++ index.js
@@ -50,18 +50,19 @@
 
 
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+  console.log(`API listening on PORT ${PORT} `);
 })
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.send('Hey this is my API running 🥳');
 })
 
 app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+  res.send('This is my about route..... ');
+})
