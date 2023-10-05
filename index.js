139e67370c455fb3c1794849b944ae5d0b9f4ae4
Asfaqe hussain <51645676+asfaqehussain@users.noreply.github.com>
2023-10-05 13:18:31 +0530

    check

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 require('dotenv').config();
 
-const Twilio = require('twilio');
+// const Twilio = require('twilio');
 const express = require('express');
 // console.log('process.env', process.env);
 
 const app = express();
 // const AccessToken = require('twilio').jwt.AccessToken;
-const AccessToken = Twilio.jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
+// const AccessToken = Twilio.jwt.AccessToken;
+// const ChatGrant = AccessToken.ChatGrant;
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
 app.get('/token/:identity', (req, res) => {
-  const identity = req.params.identity;
-  const token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY,
-    process.env.TWILIO_API_SECRET,
-  );
+  // const identity = req.params.identity;
+  // const token = new AccessToken(
+  //   process.env.TWILIO_ACCOUNT_SID,
+  //   process.env.TWILIO_API_KEY,
+  //   process.env.TWILIO_API_SECRET,
+  // );
   console.log('process.env', process.env);
 
-  token.identity = identity;
-  token.addGrant(
-    new ChatGrant({
-      serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
-    }),
-  );
+  // token.identity = identity;
+  // token.addGrant(
+  //   new ChatGrant({
+  //     serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
+  //   }),
+  // );
 
-  res.send({
-    identity: token.identity,
-    jwt: token.toJwt(),
-  });
+  // res.send({
+  //   identity: token.identity,
+  //   jwt: token.toJwt(),
+  // });
 });
 
 app.listen(3001, function () {
