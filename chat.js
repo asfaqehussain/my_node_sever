4294a3879c633ccf04f9b7906481608222070a33
Asfaqe hussain <51645676+asfaqehussain@users.noreply.github.com>
2024-08-22 00:00:25 +0530

    format and remove the unnecesory code

--- chat.js
+++ chat.js
@@ -1,38 +1,34 @@
-
 const twilio = require('twilio');
-// console.log(' twi ',twilio);
-
 
 // Fetch key, secret and ids from .env file
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const serviceSid = process.env.TWILIO_SERVICE_SID;
-const apiKey = process.env.TWILIO_API_KEY;
-const apiSecret = process.env.TWILIO_API_SECRET;
-// const client = require('twilio')(accountSid, authToken);
-
+const {
+  TWILIO_ACCOUNT_SID: accountSid,
+  TWILIO_API_KEY: apiKey,
+  TWILIO_API_SECRET: apiSecret,
+  TWILIO_SERVICE_SID: serviceSid
+} = process.env;
 
 function getToken(identity) {
-    const AccessToken = twilio.jwt.AccessToken;
-    // console.log(' Access ------', AccessToken);
-    console.log(' identity ------', identity);
-    const ChatGrant = AccessToken.ChatGrant;
+  try {
+    if (!identity) {
+      throw new Error('Identity is required');
+    }
 
-    const chatGrant = new ChatGrant({
-        serviceSid: serviceSid,
+    const { AccessToken } = twilio.jwt;
+    const { ChatGrant } = AccessToken;
+
+    const chatGrant = new ChatGrant({ serviceSid });
+    const token = new AccessToken(accountSid, apiKey, apiSecret, { 
+      identity, 
+      ttl: 3600 // token expiration time
     });
-    const token = new AccessToken(
-        accountSid,
-        apiKey,
-        apiSecret,
-        { 
-            identity: identity, // identify user uniquely 
-            ttl: 3600 // token expiration time
-        }
-    );
 
-    token.addGrant(chatGrant); 
+    token.addGrant(chatGrant);
     return token.toJwt();
+  } catch (error) {
+    console.error('Error generating token:', error.message);
+    throw error;
+  }
 }
 
-module.exports = {getToken};
+module.exports = { getToken };
